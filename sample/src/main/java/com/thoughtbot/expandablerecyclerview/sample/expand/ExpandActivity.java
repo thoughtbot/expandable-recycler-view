27caaf3a7e88d77096e35d68cace8b8ea35dc136
Amanda Hill <amandabesshill@gmail.com>
2017-01-10 14:02:46 -0800

    Update ExpandActivity sample to turn off flash animation

--- sample/src/main/java/com/thoughtbot/expandablerecyclerview/sample/expand/ExpandActivity.java
+++ sample/src/main/java/com/thoughtbot/expandablerecyclerview/sample/expand/ExpandActivity.java
@@ -3,6 +3,7 @@ package com.thoughtbot.expandablerecyclerview.sample.expand;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -26,6 +27,14 @@ public class ExpandActivity extends AppCompatActivity {
 
     RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
     LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+    // RecyclerView has some built in animations to it, using the DefaultItemAnimator.
+    // Specifically when you call notifyItemChanged() it does a fade animation for the changing
+    // of the data in the ViewHolder. If you would like to disable this you can use the following:
+    RecyclerView.ItemAnimator animator = recyclerView.getItemAnimator();
+    if (animator instanceof DefaultItemAnimator) {
+      ((DefaultItemAnimator) animator).setSupportsChangeAnimations(false);
+    }
 
     adapter = new GenreAdapter(makeGenres());
     recyclerView.setLayoutManager(layoutManager);
